d6eaaaada3849c1960152f5ff2451e2fe3bcd62c
Marco Araujo <marco.araujo.junior@gmail.com>
2015-10-22 15:15:48 -0200

    first commit

---
+++ src/SqlToMigrationServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Highideas\SqlToMigration;
+
+use Illuminate\Support\ServiceProvider;
+
+class SqlToMigrationServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
