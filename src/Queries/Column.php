e48b9803907903f57c97dd423b4e70f0694e4426
Marco Araujo <marco.araujo.junior@gmail.com>
2015-12-01 14:06:24 -0200

    Update class Column

--- src/Queries/Column.php
+++ src/Queries/Column.php
@@ -6,8 +6,9 @@ class Column
 {
     protected $column;
     protected $type;
+    protected $raw;
 
-    public function __construct($query)
+    public function __construct($column)
     {
         $output_array = [];
         preg_match("/^([a-zA-Z-_]+)\s+([\w\W]+)/i", $column, $output_array);
@@ -16,6 +17,7 @@ class Column
         }
         $this->column = $output_array[1];
         $this->type = $output_array[2];
+        $this->raw = $column;
     }
 
     public function getColumn()
@@ -26,5 +28,10 @@ class Column
     public function getType()
     {
         return $this->type;
+    }
+
+    public function isNullable()
+    {
+        return strpos(strtolower($this->raw), 'not null') === false;
     }
 }
