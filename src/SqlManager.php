ef3065a1791eb28b44f60673574121a29c30d38d
Marco Araujo <marco.araujo.junior@gmail.com>
2016-01-22 04:44:40 -0200

    Increase Tests

--- src/SqlManager.php
+++ src/SqlManager.php
@@ -3,25 +3,50 @@
 namespace Highideas\SqlToMigration;
 
 use Highideas\SqlToMigration\Queries\QueryFactory;
+use Highideas\SqlToMigration\Collections\Collection;
 
 class SqlManager
 {
     protected $rawQuery;
-    protected $query;
+    protected $queries;
+    protected $delimiter = ';';
 
-    public function __constructor($rawQuery)
+    public function __constructor($rawQuery, Collection $queries)
     {
+        var_dump('AAAA');
+        var_dump($rawQuery);
+        var_dump('AAAA');
         $this->rawQuery = $rawQuery;
-        $this->defineQuery();
+        $this->queries = $queries;
     }
 
-    protected function defineQuery()
+    protected function defineQuery($rawQuery)
     {
-        $preFormatedRawQuery = trim(strtolower($this->rawQuery));
-
+        var_dump($rawQuery);
+        $preFormatedRawQuery = trim(strtolower($rawQuery));
         if (strpos($preFormatedRawQuery, 'create table') !== false) {
-            $this->query = QueryFactory::instantiate('create', $this->rawQuery);
-            return true;
+            $query = QueryFactory::instantiate('create', $rawQuery);
+            $this->getQueries()->add($column->getTable(), $query);
         }
+    }
+
+    public function run()
+    {
+        $queries = explode($this->delimiter, $this->getRawQuery());
+        var_dump($this->getRawQuery());
+        var_dump($queries);
+        foreach ($queries as $query) {
+            $this->defineQuery($query);
+        }
+    }
+
+    public function getQueries()
+    {
+        return $this->queries;
+    }
+
+    public function getRawQuery()
+    {
+        return $this->rawQuery;
     }
 }
